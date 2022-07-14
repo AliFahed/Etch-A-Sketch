3f65b1fd53944bea77795d68c4c64136d410cba8
Ali Al-Dhib <alifahed67@gmail.com>
2022-07-15 01:21:11 +0800

    Add user input limitation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function grid(size) {
   const container = document.querySelector('.container');
+  const cells = container.querySelectorAll('div');
+  cells.forEach((div) => div.remove);
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  let num = size * size;
+  for (let i = 0; i < num; i++) {
     let cell = document.createElement('div');
     cell.style.backgroundColor = 'blue';
     container.insertAdjacentElement('beforeend', cell);
@@ -11,3 +14,11 @@ function grid(size) {
 }
 
 grid(16);
+
+function changeSize(newSize) {
+  if (newSize >= 2 && newSize <= 100) {
+    grid(newSize);
+  } else {
+    console.log("Maximum input is 100.");
+  }
+}
